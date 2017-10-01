3d779c12602514e3713b10328c2d6f8bbebb36a4
Ryan Liew <fatal-fates@hotmail.com>
2017-10-01 16:42:52 +0800

    Modified default avatar path

--- app/User.php
+++ app/User.php
@@ -58,7 +58,7 @@ class User extends Authenticatable
 
     public function getAvatarPathAttribute($avatar)
     {
-        return asset( $avatar ? 'storage/' . $avatar : "storage/avatars/default.jpg");
+        return asset( $avatar ? 'storage/' . $avatar : "/img/default.jpg");
     }
 
     public function getWinRateForRole($role_id)
