e779d1d773ee9ab29138fae7ad1b839550bab86d
Ryan Liew <fatal-fates@hotmail.com>
2017-10-15 14:57:19 +0800

    Change user repository to use first or create

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -8,14 +8,10 @@ class UserRepository {
 
 	public function findByUsernameOrCreate($userData)
 	{
-		return User::updateOrCreate(
+		return User::firstOrCreate(
 			[
 				'email' =>	$userData->email,
 				'password' => bcrypt('123456')
-			],
-			[
-				'avatar_path' => $userData->avatar,
-				'name' => $userData->name
 			]
 		);
 	}
