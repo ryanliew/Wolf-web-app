02f49ec2ced6ece426d483e6d3293d1f40fd914e
Ryan Liew <fatal-fates@hotmail.com>
2017-10-22 18:00:47 +0800

    Added patch notes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -354,6 +354,9 @@
                             <li><a href="{{ route('register') }}">注册</a></li>
                         @else
                             <li>
+                                <a href="/patches">补丁解说</a>
+                            </li>
+                            <li>
                                 <a href="/home">排行榜</a>
                             </li>
                             <li>
