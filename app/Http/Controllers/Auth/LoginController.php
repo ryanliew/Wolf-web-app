fe767c64165282f40297152c3eb1872d097ac233
Ryan Liew <fatal-fates@hotmail.com>
2018-01-10 16:09:09 +0800

    Completed player role selection through script

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/notice';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
